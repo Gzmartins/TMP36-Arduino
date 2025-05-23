146faa4e07bc86cd11b94ee069680bb85974d6da
Guilherme Martins <158292944+Gzmartins@users.noreply.github.com>
2025-05-22 21:03:28 -0300

    Adicionando codigo dos leds e buzzer

--- Arduino_work.py
+++ Arduino_work.py
@@ -2,3 +2,10 @@ const int sensorPin = A0;
 const int ledFrio = 2;
 const int ledCalor = 3;
 const int buzzer = 4;
+
+void setup() {
+  pinMode(ledFrio, OUTPUT);
+  pinMode(ledCalor, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+  Serial.begin(9600);
+}
